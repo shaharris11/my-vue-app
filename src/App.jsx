20ef40e78784be5e3b85f8262f9b432be7945518
shakira <shaharris11@yahoo.com>
2023-12-10 16:47:07 -0500

    src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log("puppyList: ", puppyList);
+  function handleClick () {
+    setPuppies(puppies);
+    setFeatPupId(featPupId);
+    }
+    
+
+  
 
   return (
-    // <>
-      <div>
-        {/* <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
+      <div className="App">
+       {
+        puppies.map((puppy) => {
+          return (
+          <p onClick={handleClick} key={puppy.id}> {puppy.name}</p>);
+        })
+       }
       </div>
-    //   {/* <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // // </> */}
-  )
+  );
 }
 
 export default App
